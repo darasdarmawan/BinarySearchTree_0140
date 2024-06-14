2c492d6b563e6295435dda32c68c838354cd8d3f
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-06-14 09:56:23 +0700

    membuat anak kanan dari node point baru menjadi null

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -28,6 +28,8 @@ public:
 		Node* newNode = new Node(element, NULL, NULL); // Allocate memmory for the new node
 		newNode->info = element;  // Assign to the data field of the new node
 		newNode->leftchild = NULL;  //Make the left child of the new node point to null
+		newNode->rightchild = NULL; // Make the right child of the new node point to null
+
 	}
 };
 
