efe63c54f9d9de961526c75914aa749f329f2148
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-06-14 10:18:13 +0700

    menambahkan void postorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -84,9 +84,23 @@ public:
 
 	void preorder(Node* ptr)
 	{
-
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
 	}
 
+	void postorder(Node* ptr)
+	{
+
+	}
 };
 
 
